424baefc96afee0e3c6f2ef53a13898ae33e2064
David McInerney <a1065639@adelaide.edu.au>
2023-03-27 13:09:30 +1030

    Unit tests for hymod and rosenbrock examples (comparison with numbers from F90 output)

---
+++ tests/testthat/test-hymod.R
@@ -0,0 +1,24 @@
+test_that('hymod', {
+
+  data("BassRiver")
+
+  tmp = rgn(simFunc=simFunc_hymod,
+            x0=c(400.,0.5,0.1,0.2,0.1),
+            xLo=c(1.,0.1,0.05,0.000001,0.000001),
+            xHi=c(1000.,2.,0.95,0.99999,0.99999),
+            simTarget=BassRiverData$Runoff.mm.day[365:length(BassRiverData$Date)],
+            cnv=setDefaultRgnConvergeSettings(dump=0, fail=0), info=rgnInfoType,
+            stateVal=c(100.0,30.0,27.0,25.0,30.0,0.0,0.0,0.0),
+            nWarmUp=365,
+            rain=BassRiverData$Rain.mm,
+            pet=BassRiverData$ET.mm)
+  error=tmp$error;message=tmp$message;x=tmp$x;info=tmp$info
+
+  # expected output based on F90 RGN hymod example
+  expect_equal(signif(x,digits=7),c(146.7564,0.3635988,0.1895957,0.99999,0.7430698))
+  expect_equal(signif(info$f,digits=7),6840.165)
+  expect_equal(info$nEval,367)
+  expect_equal(info$nIter,29)
+  expect_equal(info$termFlag,2)
+
+})
---
+++ tests/testthat/test-rosenbrock.R
@@ -0,0 +1,26 @@
+test_that('rosenbrock', {
+
+  simFunc_rosenbrock=function(x){
+    r=rep(0,2)
+    r[1] = 1.0-x[1]
+    r[2]=10.0*(x[2]-x[1]**2)
+    return(r)
+  }
+
+  tmp = rgn(simFunc=simFunc_rosenbrock,
+            x0=c(-1.0,  0.0), xLo=c(-1.5, -1.0), xHi=c( 1.5,  3.0),
+            simTarget=c(0,0),
+            cnv=setDefaultRgnConvergeSettings (dump=10, fail=0),
+            info=rgnInfoType)
+  error=tmp$error;message=tmp$message;x=tmp$x;info=tmp$info
+
+  # expected output based on F90 RGN Rosenbrock example
+  expect_equal(x,c(1,1))
+  expect_equal(info$f,0)
+  expect_equal(info$nEval,116)
+  expect_equal(info$nIter,16)
+  expect_equal(info$termFlag,2)
+
+})
+
+
