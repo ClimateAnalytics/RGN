37fc0de8dadc1a755aa8881d0afcf3dcef1ff2eb
David McInerney <a1065639@adelaide.edu.au>
2023-09-06 08:06:37 +0930

    add reference for Bass River data

--- R/BassRiverData.R
+++ R/BassRiverData.R
@@ -1,9 +1,12 @@
 #' @title Hydrological data for Bass River catchment in Victoria, Australia
 #'
 #' @name BassRiverData
+#' @description Streamflow, rainfall and PET data for Bass River catchment
+#' (227219) in Victoria, Australia. Originally obtained from Francis Chiew.
 #' @docType data
 #' @usage data(BassRiver)
-#' @references \url{XXXX}
+#' @references \url{https://github.com/eachonly/Robust-Gauss-Newton-Algorithm}
+#' \url{http://www.bom.gov.au/water/hrs/#id=227219}
 #' @format List containing numerical vectors for
 #'         precipitation (\code{Rain.mm}),
 #'         potential evapotranspiration (\code{ET.mm}),
